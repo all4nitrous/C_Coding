05d2479590e0ccb4f2eac7d147c3912173f8c0a2
all4nitrous <all4nitrous@gmail.com>
2024-12-24 11:19:15 -0500

    First C Lessons added

---
+++ Lesson_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char characterName[] = "John";
+    int characterAge = 35;
+    printf("There is a man named %s\n", characterName);
+    printf("He really liked the name %s\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+
+    return 0;
+
+}
