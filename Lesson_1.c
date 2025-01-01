897944e63400116fcc51e2b349d5f9ee63209114
all4nitrous <all4nitrous@gmail.com>
2024-12-31 21:31:45 -0500

    Unity testing corrected

--- Lesson_1.c
+++ Lesson_1.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 void print_hello_world(void) {
     printf("Hello World!\n");
 }
 
+#ifndef TESTING
 int main(void) {
     print_hello_world();
     return 0;
 }
+#endif
