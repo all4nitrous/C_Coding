db5c3515779caa6b62ab35f51d47de3cb27b6a7a
all4nitrous <all4nitrous@gmail.com>
2025-01-01 14:23:16 -0500

    Lesson_5 finished

--- Lesson_5.c
+++ Lesson_5.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h> //Needed to use math functions in VSC
 
 int main() {
 
-    printf("%d\n", 5 / 4);
+    printf("%f\n", pow(4,3));  // ^ to the power of
+    printf("%f\n", sqrt(36)); //Square root
+    printf("%f\n", ceil(36.356)); //Round up
+    printf("%f\n", floor(36.656)); //Round Down
 
     return 0;
 }
