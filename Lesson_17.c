7c3394df5a100c313881d303cb0e593c5a929bff
all4nitrous <all4nitrous@gmail.com>
2025-01-04 12:18:24 -0500

    Lesson 17 added and compiled

---
+++ Lesson_17.c
@@ -0,0 +1,15 @@
+/* Lesson on While Loops */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int index = 1;
+    while(index <= 5) {
+        printf("%d\n", index);
+        index++; //++ adds 1
+    }
+
+
+    return 0;
+}
