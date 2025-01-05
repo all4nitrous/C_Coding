31ec68183436e6e86b616eb7882db4d0b038da2f
all4nitrous <all4nitrous@gmail.com>
2025-01-05 13:12:08 -0500

    For loops lesson finished and compiled

--- Lesson_19.c
+++ Lesson_19.c
@@ -5,11 +5,15 @@
 
 int main() {
 
-    int i = 1;
+/*  int i = 1;  // same output but in the form of a while loop
     while(i <= 5){
         printf("%d\n", i);
         i++;
-    }
+    } */
+int i;
+for(i = 1; i<=5; i++) {
+    printf("%d\n", i);
+}
 
     return 0;
 }
