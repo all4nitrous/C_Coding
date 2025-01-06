bc7dc8066399fe8098e488014b555b6f5425f3e7
all4nitrous <all4nitrous@gmail.com>
2025-01-05 19:21:27 -0500

    Lesson 20 added

---
+++ Lesson_20.c
@@ -0,0 +1,16 @@
+/* 2D arrays */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int nums[3][2] = { // 3 arrays with two digits inside
+        {1, 2},
+        {3, 4},
+        {5, 6}
+    };
+
+
+
+    return 0;
+}
